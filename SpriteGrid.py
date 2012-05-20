91ce8226e9f72380f01a1bef05c85e43e6529350
Carlo Biedenharn <carlo.pwn@gmail.com>
2012-05-20 16:14:00 -0400

    renaming files

--- SpriteGrid.py
+++ SpriteGrid.py
@@ -1,20 +1,20 @@
-class ZeldaMap:
+class SpriteGrid:
   def __init__(self, width, height, gridlist):
     self.width = width
     self.height = height
     self.gridlist = gridlist
 
-  def generate_divlist(self):
-    mapstring = '<div id="zeldamap">'
+  def generate_html(self):
+    mapstring = '<div id="spritegrid">\n  '
     for n in self.gridlist:
       mapstring += '<span class="tile tile_'+ str(n) + '"></span>'
     #print mapstring + '</div>'
     files = open('TESTMAP.html','w')
-    files.write('<link rel="stylesheet" href="TESTMAP.css" type="text/css" />'+mapstring+'</div>')
+    files.write('<link rel="stylesheet" href="TESTMAP.css" type="text/css" />\n\n'+mapstring+'\n</div>')
 
   def generate_css(self):
     files = open('TESTMAP.css','w')  
-    files.write('#zeldamap {\n'+'  position:relative;\n'+ '  width:'+str(self.width*16)+'px;\n'+ '  height:'+str(self.height*16)+'px;\n'+ '  border: none;\n'+ '}\n'+ '\n'+ '.tile {\n'+ '  z-index:10;\n'+'  display:inline-block;\n'+ '  width: 16px;\n'+ '  height:16px;\n'+ '  margin: 0px;\n'+ '  padding:0px;}\n\n')
+    files.write('#spritegrid {\n'+'  position:relative;\n'+ '  width:'+str(self.width*16)+'px;\n'+ '  height:'+str(self.height*16)+'px;\n'+ '  border: none;\n'+ '}\n'+ '\n'+ '.tile {\n'+ '  z-index:10;\n'+'  display:inline-block;\n'+ '  width: 16px;\n'+ '  height:16px;\n'+ '  margin: 0px;\n'+ '  padding:0px;}\n\n')
     for n in set(self.gridlist):
       bigX,bigY = n%24, n/24
       x,y       = 17*bigX+1, 17*bigY+1
